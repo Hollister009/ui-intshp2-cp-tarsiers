20240c7738d3bef743acce5c56ad9f2d4cbcdf52
YarynaD <jaryna.m@gmail.com>
2019-01-14 15:26:41 +0200

    comments removed

--- src/app/components/AdvertisingArea/AdvertisingArea.js
+++ src/app/components/AdvertisingArea/AdvertisingArea.js
@@ -5,8 +5,6 @@ import Dynamic from './Dynamic';
 import mySanitize from '../../../utils/sanitizer';
 import './Banner.scss';
 
-// const sanitizeHtml = require('sanitize-html');
-
 export default class AdvertisingArea extends React.Component {
   constructor() {
     super();
@@ -22,40 +20,6 @@ export default class AdvertisingArea extends React.Component {
       this.setState({ htmlSnipet: clean });
     });
   }
-
-  // mySanitize(dirty) {
-  //   const myClean = sanitizeHtml(dirty, {
-  //     allowedTags: [
-  //       'b',
-  //       'i',
-  //       'em',
-  //       'strong',
-  //       'a',
-  //       'div',
-  //       'p',
-  //       'h1',
-  //       'h2',
-  //       'h3',
-  //       'h4',
-  //       'h5',
-  //       'h6',
-  //       'img',
-  //       'style'
-  //     ],
-  //     allowedAttributes: { a: ['href'], img: ['src'] },
-  //     allowedIframeHostnames: ['www.youtube.com'],
-  //     allowedClasses: {
-  //       p: ['item__description', 'simple'],
-  //       div: ['baner', 'item', 'item__description', 'priceblock'],
-  //       h3: ['item__name'],
-  //       h2: ['priceblock__price'],
-  //       img: ['banner__image', 'priceblock__cart']
-  //     },
-  //     nonTextTags: ['style']
-  //   });
-
-  //   return myClean;
-  // }
 
   render() {
     const mySnipet = this.state;
