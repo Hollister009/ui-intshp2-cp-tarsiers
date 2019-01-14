1402eb01136f13e810161c1fc32e64fe5c774615
YarynaD <jaryna.m@gmail.com>
2019-01-14 17:04:52 +0200

    UIO-12 media reference fixed + UIO-17 snackbar message moved to appConfig

--- src/app/components/JoinUs/JoinUs.js
+++ src/app/components/JoinUs/JoinUs.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Snackbar from '../Snackbar/Snackbar';
+import appConfig from '../../../config/appConfig';
 import mailicon from '../../../assets/mailicon.png';
 import './Joinus.scss';
 
@@ -25,9 +26,9 @@ class JoinUs extends Component {
   };
 
   showSnackbarHandler = () => {
-    this.snackbarRef.current.openSnackBar(
-      'You are registered now :) Have a great shopping!'
-    );
+    const { message } = appConfig.joinUsSnackbar;
+
+    this.snackbarRef.current.openSnackBar(message);
   };
 
   canBeSubmitted = () => {
