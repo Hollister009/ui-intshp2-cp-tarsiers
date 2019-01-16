73f795795c64b5acd9cee1e90a1d3eb92bdc056d
YarynaD <jaryna.m@gmail.com>
2019-01-16 14:55:35 +0200

    get products data from api

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Content from './components/Content/Content';
+import HttpService from '../utils/http.service';
 import '../styles/index.scss';
 
-const App = () => (
-  <Router>
-    <HashRouter>
-      <>
-        <Header />
-        <Content className="content" />
-        <Footer />
-      </>
-    </HashRouter>
-  </Router>
-);
+class App extends Component {
+  componentDidMount() {
+    HttpService.get('/api/products').then(res =>
+      this.setState({ products: res }, () => {
+        console.log('state', this.state);
+      })
+    );
+  }
+
+  render() {
+    return (
+      <Router>
+        <HashRouter>
+          <>
+            <Header />
+            <Content className="content" />
+            <Footer />
+          </>
+        </HashRouter>
+      </Router>
+    );
+  }
+}
 
 const mapStateToProps = state => ({ ...state });
 
