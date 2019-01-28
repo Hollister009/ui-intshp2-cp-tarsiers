b8498b07e0c78f67c12fbe5312162bd30a6e2606
Hollister009 <hipnos.az@gmail.com>
2019-01-28 22:50:07 +0200

    feature/uio-28 added disabled state for wishlist button

--- src/app/shared/ProductItem/ProductItem.js
+++ src/app/shared/ProductItem/ProductItem.js
@@ -1,17 +1,13 @@
-/* eslint-disable no-console */
 /* eslint-disable object-curly-newline */
 import React, { Component } from 'react';
 import { bool } from 'prop-types';
 
 import productType from '../../types';
-import HttpService from '../../../utils/http.service';
-import appConfig from '../../../config/appConfig';
 import { ViewFrontFull, ViewDetailsFull } from './viewFull';
 import { ViewCartSmall, ViewInfoSmall } from './viewSmall';
 import './ProductItem.scss';
 
 const CN = 'product-item';
-const { addToWishList, removeFromWishList } = appConfig.apiResources;
 
 class ProductItem extends Component {
   static propTypes = {
@@ -34,51 +30,19 @@ class ProductItem extends Component {
 
   showDetails = () => this.setState({ showDetails: true });
 
-  addToWishList = id => {
-    const { addToWishListItem, isAddedtoWishList } = this.props;
-
-    HttpService.post(addToWishList, { productId: id })
-      .then(res => {
-        if (res.status === 200 && !isAddedtoWishList) {
-          addToWishListItem(id);
-          console.log(`Added to the WishList: ${id}`);
-        }
-      })
-      .catch(error => console.log(error));
-  };
-
-  removeFromWishList = id => {
-    const { removeFromWishListItem } = this.props;
-
-    HttpService.post(removeFromWishList, { productId: id })
-      .then(res => {
-        if (res.status === 200) {
-          removeFromWishListItem(id);
-          console.log(`Removed from the WishList: ${id}`);
-        }
-      })
-      .catch(error => console.log(error));
-  };
-
-  toggleWishList = (e, id) => {
-    e.preventDefault();
-    const { isAddedtoWishList } = this.props;
-
-    const cb = !isAddedtoWishList
-      ? this.addToWishList
-      : this.removeFromWishList;
-
-    cb(id);
-  };
-
   render() {
     const { showDetails } = this.state;
-    const { data, extended, isAddedtoWishList } = this.props;
+    const {
+      data,
+      extended,
+      isAddedtoWishList,
+      addToWishListItem,
+      removeFromWishListItem
+    } = this.props;
     const { src, title, price } = data;
 
     return extended ? (
       <div
-        ref={this.itemRef}
         className={`${CN} ${CN}--full`}
         onMouseEnter={this.showDetails}
         onMouseLeave={this.showFront}
@@ -86,16 +50,16 @@ class ProductItem extends Component {
         {showDetails ? (
           <ViewDetailsFull
             {...data}
-            clickHandler={this.toggleWishList}
+            addToWishListItem={addToWishListItem}
+            removeFromWishListItem={removeFromWishListItem}
             wished={isAddedtoWishList}
           />
         ) : (
-          <ViewFrontFull {...data} />
+          <ViewFrontFull src={src} title={title} price={price} />
         )}
       </div>
     ) : (
       <div
-        ref={this.itemRef}
         className={`${CN} ${CN}--small`}
         onMouseEnter={this.showDetails}
         onMouseLeave={this.showFront}
