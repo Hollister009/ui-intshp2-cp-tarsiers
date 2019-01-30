dcc4563d51457fd3c46b67ee18b406f9500ba322
Hollister009 <hipnos.az@gmail.com>
2019-01-31 00:55:18 +0200

    JoinUs test work in progress

--- src/app/components/JoinUs/JoinUs.test.js
+++ src/app/components/JoinUs/JoinUs.test.js
@@ -4,17 +4,24 @@ import JoinUs from './JoinUs';
 
 describe('<JoinUs />', () => {
   it('should match its snapshot', () => {
-    const props = { email: '' };
-    const wrapper = shallow(<JoinUs {...props} />);
+    const wrapper = shallow(<JoinUs />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
-  it('should handle email input changes', () => {
-    const props = {};
-    const wrapper = mount(<JoinUs {...props} />);
+  it('should be able to call handleEmailChange and set state', () => {
+    const evt = { target: { value: 'example@mail.com' } };
+    const wrapper = shallow(<JoinUs />);
 
-    wrapper.setProps({ onChange: 'handleEmailChange' });
-    expect(wrapper.props().onChange).toBe('handleEmailChange');
+    wrapper.find('#email').simulate('change', evt);
+    expect(wrapper.state().email).toBe(evt.target.value);
+  });
+
+  xit('should be able to call showSnackbarHandler with message', () => {
+    const wrapper = mount(<JoinUs />);
+    // const spy = jest.spyOn(wrapper.instance(), 'showSnackbarHandler');
+
+    wrapper.find('[type="submit"]').simulate('click');
+    expect(wrapper.ref('snackbarRef'));
   });
 });
