55999109358de114eedfd014a8d26684ef122cba
YarynaD <jaryna.m@gmail.com>
2019-02-06 15:48:42 +0200

    UIO-45/jest fixes

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,12 @@ module.exports = {
   setupFiles: ['<rootDir>/.utils/setupTest.js'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
-    '.*\\.js$':
-      '<rootDir>/node_modules/jest-runtime/build/script_transformer.js'
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform'
   },
   moduleNameMapper: {
     '^.+\\.(jpg|jpeg|gif|png|mp4|mkv|avi|webm|swf|wav|mid)$':
+      '<rootDir>/.utils/file.stub.js',
+    'react-redux-notify/dist/ReactReduxNotify.css':
       '<rootDir>/.utils/file.stub.js'
   },
   transformIgnorePatterns: ['./node_modules/'],
