927daed42641dfa831d01c48aa5576d76bc96d47
YarynaD <jaryna.m@gmail.com>
2019-02-06 13:01:40 +0200

    UIO-45/jest config changes

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
   setupFiles: ['<rootDir>/.utils/setupTest.js'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform'
+    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+    '.*\\.js$':
+      '<rootDir>/node_modules/jest-runtime/build/script_transformer.js'
   },
   moduleNameMapper: {
     '^.+\\.(jpg|jpeg|gif|png|mp4|mkv|avi|webm|swf|wav|mid)$':
