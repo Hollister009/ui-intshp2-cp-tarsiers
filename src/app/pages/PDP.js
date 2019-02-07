254fd6858e7121613d0b27370f90e7ebecba37e6
SofiMat <sofiyamatviishyn@gmail.com>
2019-02-07 11:25:01 +0200

    delete h3 from pdp

--- src/app/pages/PDP.js
+++ src/app/pages/PDP.js
@@ -3,7 +3,6 @@ import ProductDetailsContainer from '../components/ProductDetails/ProductDetails
 
 const ProductDetailsPage = ({ match }) => (
   <div>
-    <h3>{match.params.productId}</h3>
     <div className="container">
       <ProductDetailsContainer id={match.params.productId} />
     </div>
