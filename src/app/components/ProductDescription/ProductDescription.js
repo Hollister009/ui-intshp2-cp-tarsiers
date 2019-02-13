5d454673cb71144629c418e041ebe8899dc96fc9
SofiMat <sofiyamatviishyn@gmail.com>
2019-02-13 14:45:22 +0200

    removed some imports

--- src/app/components/ProductDescription/ProductDescription.js
+++ src/app/components/ProductDescription/ProductDescription.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import HttpService from '../../../utils/http.service';
 import productType from '../../../types';
 import appConfig from '../../../config/appConfig';
 import styles from './ProductDescription.module.scss';
