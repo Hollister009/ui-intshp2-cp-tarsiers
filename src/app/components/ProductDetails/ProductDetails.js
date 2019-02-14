c5f99d994570de97251a952abc729668117d4339
moskdev <molchanovsk@gmail.com>
2019-02-14 14:34:17 +0200

    product details UPD

--- src/app/components/ProductDetails/ProductDetails.js
+++ src/app/components/ProductDetails/ProductDetails.js
@@ -1,59 +1,32 @@
 import React, { Component } from 'react';
 import ProductDescription from '../ProductDescription/ProductDescription';
 import RelatedProducts from '../RelatedProducts/RelatedProducts';
-import HttpService from '../../../utils/http.service';
+import Spinner from '../../shared/Spinner';
 
 class ProductDetails extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { relatedProducts: [] };
-  }
-
   componentDidMount() {
-    this.forceUpdate();
     window.scrollTo(0, 0);
   }
-
-  componentDidUpdate() {
-    const { products, id } = this.props;
-    const { relatedProducts } = this.state;
-    const item = products.find(el => el._id === id);
-
-    if (item && !relatedProducts.length) {
-      const params = {
-        brands: [],
-        available: null,
-        price: { min: 0, max: 1000 },
-        category: item.category,
-        tag: item.tag,
-        skip: 0,
-        limit: 1000
-      };
-
-      this.getFilteredProducts({ params }).then(res => {
-        this.setState({ relatedProducts: res.data });
-      });
-    }
-  }
-
-  getFilteredProducts = params =>
-    HttpService.get('/api/filtered-products', params);
 
   render() {
     const { id, products, wishlist } = this.props;
     const item = products.find(el => el._id === id);
-    const { relatedProducts } = this.state;
 
     return (
       <div className="container">
-        <ProductDescription item={item} />
-        {relatedProducts.length ? (
-          <RelatedProducts
-            item={item}
-            products={relatedProducts}
-            wishlist={wishlist}
-          />
-        ) : null}
+        {item ? (
+          <React.Fragment>
+            <ProductDescription item={item} />
+            <RelatedProducts
+              item={item}
+              products={products}
+              wishlist={wishlist}
+              id={id}
+            />
+          </React.Fragment>
+        ) : (
+          <Spinner height="80vh" />
+        )}
       </div>
     );
   }
