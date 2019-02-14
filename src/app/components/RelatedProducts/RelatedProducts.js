f08f6ea6c75ecfde6f1eefce37d20247df95d2f8
moskdev <molchanovsk@gmail.com>
2019-02-14 23:42:57 +0200

    Feature/uio 33/related products (#42)
    * related products fix
    
    * product details UPD
    
    * snapshots update

--- src/app/components/RelatedProducts/RelatedProducts.js
+++ src/app/components/RelatedProducts/RelatedProducts.js
@@ -1,52 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import productType from '../../types/index';
 import Carousel from '../../shared/Carousel/index';
 import ProductItemContainer from '../../shared/ProductItem/ProductItemContainer';
+import HttpService from '../../../utils/http.service';
 
 import './RelatedProducts.scss';
 
 const title = 'Related Products';
 
-const RelatedProducts = props => {
-  const titleArr = title.split(' ');
-  const { products, wishlist } = props;
-  const extended = true;
-  const isAddedtoWishList = id => wishlist.includes(id);
+class RelatedProducts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { relatedProducts: [] };
+  }
 
-  const list =
-    products &&
-    products.map(el => (
-      <ProductItemContainer
-        isAddedtoWishList={isAddedtoWishList(el._id)}
-        extended={extended}
-        key={el._id}
-        data={el}
-      />
-    ));
+  isAddedtoWishList = id => {
+    const { wishlist } = this.props;
 
-  return (
-    <section className="container related-products">
-      <div className="section_heading">
-        <h2>
-          <span className="highlighted">{titleArr[0]}</span>
-          &nbsp;
-          {titleArr[1]}
-        </h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry
-        </p>
-      </div>
-      <Carousel data={products} extended>
-        {list}
-      </Carousel>
-    </section>
-  );
-};
+    return wishlist.includes(id);
+  };
+
+  getFilteredProducts = params =>
+    HttpService.get('/api/filtered-products', params);
+
+  componentDidMount = () => {
+    this.getRelatedProducts();
+  };
+
+  getRelatedProducts = () => {
+    const { item } = this.props;
+
+    const params = {
+      brands: [],
+      available: null,
+      price: { min: 0, max: 1000 },
+      category: item.category,
+      tag: item.tag,
+      skip: 0,
+      limit: 1000
+    };
+
+    this.getFilteredProducts({ params }).then(res => {
+      this.setState({ relatedProducts: res.data });
+    });
+  };
+
+  render() {
+    const titleArr = title.split(' ');
+    const { relatedProducts } = this.state;
+    const extended = true;
+    const list =
+      relatedProducts &&
+      relatedProducts.map(el => (
+        <ProductItemContainer
+          isAddedtoWishList={this.isAddedtoWishList(el._id)}
+          extended={extended}
+          key={el._id}
+          data={el}
+        />
+      ));
+
+    return (
+      <section className="container related-products">
+        <div className="section_heading">
+          <h2>
+            <span className="highlighted">{titleArr[0]}</span>
+            &nbsp;
+            {titleArr[1]}
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry
+          </p>
+        </div>
+        <Carousel data={relatedProducts} extended>
+          {list}
+        </Carousel>
+      </section>
+    );
+  }
+}
 
 RelatedProducts.propTypes = {
-  products: PropTypes.arrayOf(productType).isRequired,
   wishlist: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
