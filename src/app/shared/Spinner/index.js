ee802fef6c461dd5c440799b849c5b0911c00686
moskdev <molchanovsk@gmail.com>
2019-02-18 00:29:30 +0200

    Spinner reimplementation (#43)
    * initial commit
    
    * snapshots UPD
    
    * Moved style inside component

--- src/app/shared/Spinner/index.js
+++ src/app/shared/Spinner/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Spinner.scss';
 
-const Spinner = () => (
-  <div className="spinner">
-    <i className="fas fa-sun" />
-  </div>
-);
+const Spinner = props => {
+  const { height } = props;
+  const spinnerStyle = { height };
+
+  return (
+    <div className="spinner-container" style={spinnerStyle}>
+      <div className="spin-position">
+        <div className="spinner">
+          <i className="fas fa-sun" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Spinner;
