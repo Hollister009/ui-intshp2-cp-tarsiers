f80b4730af1e398d4212a70106106892e8624822
SofiMat <sofiyamatviishyn@gmail.com>
2019-02-21 16:21:15 +0200

    created cart container

--- src/app/components/Cart/CartContainer.js
+++ src/app/components/Cart/CartContainer.js
@@ -1,30 +1,20 @@
 import { connect } from 'react-redux';
 import { createNotification } from 'react-redux-notify';
-import {
-  addToWishList,
-  removeFromWishList,
-  addToCart,
-  removeFromCart,
-  orderNow
-} from '../../../actions';
-import ProductDescription from './ProductDescription';
+import { removeFromCart, orderNow } from '../../actions';
+import Cart from './Cart';
 
 const mapStateToProps = state => ({
-  wishlist: state.wishlist.wishlist,
   cart: state.cart,
   orders: state.orders
 });
 const mapDispatchToProps = dispatch => ({
-  addToWishListItem: data => dispatch(addToWishList(data)),
-  removeFromWishListItem: data => dispatch(removeFromWishList(data)),
-  addToCart: data => dispatch(addToCart(data)),
   removeFromCart: data => dispatch(removeFromCart(data)),
   createNotification: config => dispatch(createNotification(config)),
   orderNowItem: data => dispatch(orderNow(data))
 });
-const ProductDescriptionContainer = connect(
+const CartContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(ProductDescription);
+)(Cart);
 
-export default ProductDescriptionContainer;
+export default CartContainer;
