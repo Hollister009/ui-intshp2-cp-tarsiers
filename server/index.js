f946ce559a6fcad9be058c467da59637f96c0727
EdGrLab <grabchak1999@gmail.com>
2019-02-22 14:30:26 +0200

    moved server and client to the seperate folders

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const router = require('./router');
 
 const app = express();
 
+const port = process.env.PORT || config.PORT;
+
 function clientErrorHandler(err, req, res, next) {
   if (req.xhr) {
     res.status(500).send({ error: 'Something failed!' });
@@ -17,17 +19,25 @@ function clientErrorHandler(err, req, res, next) {
 
 app.use(clientErrorHandler);
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.use('/', router);
+app.use('/api', router);
 
-app.listen(config.PORT, () => {
-  console.log(`listening on ${config.PORT}`);
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app
+    .use(express.static(path.resolve(__dirname, '../client/build')))
+    .get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+    );
+}
 
 module.exports = app;
