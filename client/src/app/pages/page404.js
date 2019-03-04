c129adbdd623055ae2d26028b922b8229cc72c2f
Sophia Matviishyn <sofiyamatviishyn@gmail.com>
2019-03-04 09:44:40 +0200

    Styled not found page (#62)

--- client/src/app/pages/page404.js
+++ client/src/app/pages/page404.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { TOGGLE_HEADER_AND_FOOTER_VISIBILITY } from '../actions';
+import '../../styles/404page.scss';
 
 class Page404 extends Component {
   componentDidMount() {
@@ -16,7 +18,27 @@ class Page404 extends Component {
   }
 
   render() {
-    return <div>Page is not found</div>;
+    return (
+      <div className="container">
+        <div className="not_found_block">
+          <div className="nf_heading">
+            <h1 className="not_found_header">
+              Sorry! This page is not found. Come back
+              <Link to="/" className="home">
+                Home!
+              </Link>
+            </h1>
+          </div>
+          <div>
+            <img
+              src="https://res.cloudinary.com/so/image/upload/v1551445566/logos/oie_11438wBPVT3CE.gif"
+              alt="Not found cat"
+              className="not_found_img"
+            />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
