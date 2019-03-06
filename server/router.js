d6a3cf776affabe2412b0bbbc3b5fa0e52281bef
Hollister009 <hipnos.az@gmail.com>
2019-03-06 10:06:05 +0200

    [UIO-55] server files changes

--- server/router.js
+++ server/router.js
@@ -2,30 +2,41 @@ const express = require('express');
 
 const router = express.Router();
 
+const options = require('./options');
 const productsController = require('./controllers/products.controller');
 const wishListController = require('./controllers/wishList.controller');
 const cartListController = require('./controllers/cart.controller');
 const checkoutController = require('./controllers/checkout.controller');
-const controller = require('./controller');
 
-router.get('/products', productsController.getProducts);
-router.get('/filtered-products', productsController.getFilteredProducts);
-router.get('/baner', controller.getBanner);
-router.get('/promotions', controller.getPromotions);
-router.get('/killswitch', controller.getKillswitch);
+// Products
+router
+  .get('/products', productsController.getProducts)
+  .get('/filtered-products', productsController.getFilteredProducts);
 
-router.get('/wish-list', wishListController.getWishList);
-router.post('/add-to-wish-list', wishListController.addToWishList);
-router.post('/remove-from-wish-list', wishListController.removeFromWishList);
+// WishList
+router
+  .get('/wish-list', wishListController.getWishList)
+  .post('/add-to-wish-list', wishListController.addToWishList)
+  .post('/remove-from-wish-list', wishListController.removeFromWishList);
 
-router.get('/cart', cartListController.getCartList);
-router.post('/add-to-cart', cartListController.addToCartList);
-router.post('/remove-from-cart', cartListController.removeFromCartList);
+// Cart
+router
+  .get('/cart', cartListController.getCartList)
+  .post('/add-to-cart', cartListController.addToCartList)
+  .post('/remove-from-cart', cartListController.removeFromCartList);
 
-router.post('/payment', checkoutController.payment);
-router.get('/success', checkoutController.onSuccess);
-router.get('/cancel', checkoutController.onCancel);
+// Checkout
+router
+  .post('/payment', checkoutController.payment)
+  .get('/success', checkoutController.onSuccess)
+  .get('/cancel', checkoutController.onCancel);
 
-router.get('*', controller.notFound);
+// Options
+router
+  .get('/baner', options.getBanner)
+  .get('/promotions', options.getPromotions)
+  .get('/killswitch', options.getKillswitch);
+
+router.get('*', options.NotFound);
 
 module.exports = router;
