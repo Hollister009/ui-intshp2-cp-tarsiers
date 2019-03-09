7dc0bc93ccc05df16192ef258bb30894c7eba5c7
Hollister009 <hipnos.az@gmail.com>
2019-03-09 20:20:21 +0200

    [UIO-60] small changes

--- client/src/app/shared/PayPalButton/index.js
+++ client/src/app/shared/PayPalButton/index.js
@@ -2,24 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './paypal.module.scss';
 
-const PayPalButton = props => {
-  const { handleClick } = props;
-
-  return (
-    <button
-      type="submit"
-      title="checkout"
-      className={styles.paypal_btn}
-      onClick={() => handleClick()}
-    >
-      <i className={styles.logo_alt} />
-      <p className={styles.paypal_txt}>
-        <span>Pay</span>
-        <span>Pal</span>
-      </p>
-    </button>
-  );
-};
+const PayPalButton = ({ handleClick }) => (
+  <button
+    type="submit"
+    title="checkout"
+    className={styles.paypal_btn}
+    onClick={() => handleClick()}
+  >
+    <i className={styles.logo_alt} />
+    <p className={styles.paypal_txt}>
+      <span>Pay</span>
+      <span>Pal</span>
+    </p>
+  </button>
+);
 
 PayPalButton.propTypes = {
   handleClick: PropTypes.func.isRequired
