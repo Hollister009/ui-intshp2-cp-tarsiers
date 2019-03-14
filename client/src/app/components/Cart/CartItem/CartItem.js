2f3e8f252779c9c9314b80695e9c32c274c6ba83
Hollister009 <hipnos.az@gmail.com>
2019-03-15 01:26:44 +0200

    [last] added a bit of styling

--- client/src/app/components/Cart/CartItem/CartItem.js
+++ client/src/app/components/Cart/CartItem/CartItem.js
@@ -139,11 +139,11 @@ class CartItem extends Component {
     return (
       <React.Fragment>
         <button type="button" onClick={this.increment} data-type="increment">
-          +
+          {String.fromCharCode(43)}
         </button>
-        {chosenQuantity}
+        <span>{chosenQuantity}</span>
         <button type="button" onClick={this.decrement} data-type="decrement">
-          -
+          {String.fromCharCode(8722)}
         </button>
       </React.Fragment>
     );
